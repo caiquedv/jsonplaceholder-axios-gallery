1c619984bc7f318e6b3acdd3eeef032f5f3c59c9
Caique Arruda <101219858+caiquedv@users.noreply.github.com>
2022-09-15 11:39:06 -0300

    New base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react_photo-gallery/',
+  base: '/jsonplaceholder-axios-gallery/',
   plugins: [react()]
 })
